87b923cc9e8e92dbbc858545bd9ea35410a8eca4
Jay <149704927+jayy0719@users.noreply.github.com>
2024-11-11 12:26:50 -0500

    Trying out conditionals again for Java

---
+++ Java/keywords/src/Review.java
@@ -0,0 +1,49 @@
+public class Review {
+    // review of topics so far
+
+    /* Primitive Types:
+    char, - any multiplication will return a bit num, to concatenate you must use an
+    empty string
+
+    byte,short,int,long - defaults to int, anything else must be type declared
+    float,double - defaults to double, must type declare float
+
+    you can type cast as long as it's in the range of the smaller typing
+    either (data type) or is letter after the num ex. (byte) 190b;
+
+
+    Each comes with a Wrapper class that is the full name + a .
+    Can change the primitives using this
+
+    Non-Primitive:
+    String ( denoted with a capital letter)
+
+
+    And &&,
+    Or ||
+
+    If {
+    }else if{
+    }else{}
+
+
+
+    Methods:
+    3 main goals :
+    Reusability
+    Reduce Code duplication
+    Achieve different Results
+
+    to declare a method
+    Must use all parameters set
+
+    public static void methodName(parameters) - for non returns
+    public static datatype methodName(parameters){
+        return;
+    } - for a returning set of data
+    ***must be the type of the returning data***
+
+
+
+     */
+}
